042a9699408dca4231ce857352e0685e872b6904
Aiden Chang <asdf62210@gmail.com>
2023-12-12 22:11:13 +0800

    Zhongshan Home - v1 Complete

---
+++ app/src/main/java/com/example/compose_bottomnavigationbar/ui/theme/screens/MainScreen.kt
@@ -0,0 +1,143 @@
+package com.example.compose_bottomnavigationbar.ui.theme.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Face
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+/**
+ * Created by AidenChang 2023/12/07
+ */
+
+data class NavItemState(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val hasBadge: Boolean,
+    val badgeNum: Int
+)
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(modifier: Modifier) {
+    val items = listOf(
+        NavItemState(
+            title = "Home",
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home,
+            hasBadge = false,
+            badgeNum = 0
+        ),
+        NavItemState(
+            title = "Inbox",
+            selectedIcon = Icons.Filled.Email,
+            unselectedIcon = Icons.Outlined.Email,
+            hasBadge = false,
+            badgeNum = 10
+        ),
+        NavItemState(
+            title = "Account",
+            selectedIcon = Icons.Filled.Face,
+            unselectedIcon = Icons.Outlined.Face,
+            hasBadge = true,
+            badgeNum = 0
+        )
+    )
+    var bottomNavState by rememberSaveable {
+        mutableStateOf(0)
+    }
+    Scaffold(
+        bottomBar = {
+            NavigationBar(
+                modifier = modifier
+                    .padding(10.dp)
+                    .clip(RoundedCornerShape(20.dp)),
+                containerColor = Color(0xFFE0A9A5)
+            ) {
+                items.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        selected = bottomNavState == index,
+                        onClick = { bottomNavState = index },
+                        icon = {
+                            BadgedBox(
+                                badge = {
+                                    if (item.hasBadge) Badge {}
+                                    if (item.badgeNum != 0) Badge {
+                                        Text(text = item.badgeNum.toString())
+                                    }
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = if (bottomNavState == index) {
+                                        item.selectedIcon
+                                    } else item.unselectedIcon,
+                                    contentDescription = item.title
+                                )
+                            }
+                        },
+                        label = {
+                            Text(text = item.title)
+                        },
+                        colors = NavigationBarItemDefaults.colors(
+                            selectedIconColor = Color(0xFF552A27),
+                            selectedTextColor = Color(0xFF63332F),
+                            indicatorColor = Color(0xFFBB7E7A)
+                        )
+                    )
+                }
+            }
+        }
+    ) { contentPadding ->
+        Column(
+            modifier = modifier
+                .padding(contentPadding)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = items[bottomNavState].title,
+                fontWeight = FontWeight.ExtraBold,
+                fontSize = 44.sp
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MainScreen(Modifier)
+}
